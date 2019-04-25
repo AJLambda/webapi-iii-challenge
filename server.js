7112dbba210763077a7cea6d7a4610e00c8e1fbb
AJLambda <apj2277@gmail.com>
2019-04-25 15:02:46 -0700

    fixed env

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ server.use("/api/posts", postsRouter);
 server.get("/", (req, res) => {
   //this function is a request handler. It is also middleware.
   //request and response are positional arguments.
-  res.send("its alive!", { messageOfTheDay: process.env.MOTD }); // .send is a method of the response object. This sends a quick response back to the client
+  res.status(200).json({ messageOfTheDay: process.env.MOTD }); // .send is a method of the response object. This sends a quick response back to the client
 });
 
 // export default server
